848155f5d3ac7bc0813686c9583660dfb3f093c6
Sena <Sena@DESKTOP-4IMMO6J>
2023-02-14 17:27:13 +0300

    Add project files.

---
+++ SanTechMakina/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanTechMakina.Models;
+using System.Diagnostics;
+
+namespace SanTechMakina.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<GalleryImage> images = new List<GalleryImage>();
+
+            DirectoryInfo d = new DirectoryInfo("././wwwroot/Img");
+
+            FileInfo[] Files = d.GetFiles();
+           
+            foreach (FileInfo file in Files)
+            {
+                GalleryImage image = new GalleryImage()
+                {
+                    Id = new Guid(),
+                    Name = file.Name,
+                    Url = file.FullName
+                };
+
+                images.Add(image);
+            }
+
+            return View(images);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Gallery()
+        {
+            List<GalleryImage> images = new List<GalleryImage>();
+
+            DirectoryInfo d = new DirectoryInfo("././wwwroot/Img");
+
+            FileInfo[] Files = d.GetFiles(); 
+            string str = "";
+
+            foreach (FileInfo file in Files)
+            {
+                str = str + ", " + file.Name;
+            }
+
+            return View(images); 
+        }
+
+
+    }
+}
---
+++ SanTechMakina/Models/GalleryImage.cs
@@ -0,0 +1,10 @@
+﻿namespace SanTechMakina.Models
+{
+    public class GalleryImage
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+    }
+}
